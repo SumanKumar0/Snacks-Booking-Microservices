42f5ade39993e71ad7520a8ef3aaf1329dc1149d
Suman Kumar <kr.suman023@gmail.com>
2023-08-22 10:03:12 +0530

    initial project service created

---
+++ Menu-Services/src/main/java/com/example/demo/MenuServicesApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenuServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MenuServicesApplication.class, args);
+	}
+
+}
---
+++ Register-Services/src/main/java/com/example/demo/RegisterServicesApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegisterServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegisterServicesApplication.class, args);
+	}
+
+}
